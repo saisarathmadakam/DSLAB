4f4ff59e530ee4fbcb3a0338c4e90ca5e823ab48
saisarathmadakam <160226471+saisarathmadakam@users.noreply.github.com>
2024-02-16 16:33:01 +0530

    Create reverse function.c

---
+++ reverse function.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int rev(int a[],int n)
+{
+int i;
+printf("enter the size of array");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+printf("\nenter the array elements");
+for(i=0;i<n;i++)
+printf("\nenter the %d elements=%d",i,a[i]);
+printf("\nenter the reverse array elements");
+for(i=n;i>=0;i--)
+printf("enter the %d elements=%d",i,a[i]);
+}
+int main()
+{
+ int a[100],n;
+ rev(a,n);
+ }
